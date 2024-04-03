fa12eb183f4933c266af2a1ef59eca7bddb59183
smngvlkz <asmdigital14@gmail.com>
2024-04-03 04:35:46 -0700

    My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Square module.
+
+It defines the square class that describes a square class.
+"""
+
+class Square:
+    """
+    The Square class.
+    defindes a square
+    """
+    pass
