86c7893a29fc4be175893db7424d60557c547264
smngvlkz <asmdigital14@gmail.com>
2024-04-16 08:23:22 -0700

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Student:
+    """
+    Defines a student by a public instance attributes:
+    first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantaition with first_name, last_name, and age.
+
+        Args:
+            first_name: Student's first name.
+            last_name: Student's last name.
+            age: Student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance
+
+        Returns:
+            dict: Dictionary representation of Student instance.
+        """
+        return self.__dict__
