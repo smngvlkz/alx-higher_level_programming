2c2fa3cf2684feacd3923273c554123c87ac577d
smngvlkz <asmdigital14@gmail.com>
2024-04-18 13:06:02 -0700

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
-"""Base class Module."""
-
+import json
+"""
+Base class Module.
+"""
 class Base:
-    """ Base class. """
+    """
+    Base class.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Constructor."""
+        """
+        Constructor.
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        JSON string representation of list_dictionaries returned.
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -25,5 +25,16 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b1.id, 8)
         self.assertEqual(b2.id, 3)
 
+    def test_to_json_string(self):
+        """
+        to_json_string static method test case.
+        """
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+        list_dicts = [{'id': 1, 'size': 10, 'x': 2, 'y': 1}]
+        json_str = Base.to_json_string(list_dicts)
+        self.assertEqual(json_str, '[{"id": 1, "size": 10, "x": 2, "y": 1}]')
+
 if __name__ == "__main__":
     unittest.main()
