3f87daa5179d51fc82dd7b0b1cb9ce63ec33d679
smngvlkz <asmdigital14@gmail.com>
2024-04-18 13:44:35 -0700

    JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,3 +27,14 @@ class Base:
         if list_dictionaries is None or not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        JSON string representation of list_obj to a file written.
+        """
+        filename = f"{cls.__name__}.json"
+        list_dicts = [obj.to_dictionary() for obj in list_objs] if list_objs else []
+        json_string = cls.to_json_string(list_dicts)
+        with open(filename, 'w') as f:
+            f.write(json_string)
--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 import unittest
+from models.rectangle import Rectangle
 from models.base import Base
+
+import os
+import json
 
 class TestBase(unittest.TestCase):
     """
     Base class test case.
     """
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
 
     def test_no_id(self):
         """
@@ -23,7 +30,7 @@ class TestBase(unittest.TestCase):
         b1 = Base(8)
         b2 = Base()
         self.assertEqual(b1.id, 8)
-        self.assertEqual(b2.id, 3)
+        self.assertEqual(b2.id, 1)
 
     def test_to_json_string(self):
         """
@@ -36,5 +43,22 @@ class TestBase(unittest.TestCase):
         json_str = Base.to_json_string(list_dicts)
         self.assertEqual(json_str, '[{"id": 1, "size": 10, "x": 2, "y": 1}]')
 
+    def test_save_to_file(self):
+        """
+        save_to_file class method test case.
+        """
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+
+        with open("Rectangle.json", "r") as file:
+            list_objs = json.load(file)
+
+        self.assertEqual(list_objs, [r1.to_dictionary(), r2.to_dictionary()])
+
+        try:
+            os.remove("Rectangle.json")
+        except FileNotFoundError:
+            pass
 if __name__ == "__main__":
     unittest.main()
