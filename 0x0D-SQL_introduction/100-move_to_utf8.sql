12ed43905997f7e133e693eedfad440433dfe606
smngvlkz <asmdigital14@gmail.com>
2024-05-21 03:58:49 -0700

    Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- Converts hbtn_0c_0 database to UTF8
+-- table: first_name to utf8
+-- field: name to utf8
+
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
