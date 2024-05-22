dc7d89b57bfe2577057c5ef33f5d182fc69504d3
smngvlkz <asmdigital14@gmail.com>
2024-05-22 03:26:18 -0700

    No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all shows contained in database without a genre linked
+
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, genre_id;
