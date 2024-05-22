011d93539b527f952977525b03fe7444763676a6
smngvlkz <asmdigital14@gmail.com>
2024-05-22 01:33:57 -0700

    Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Lists all cities containd in the database 'hbtn_0d_usa'
+
+SELECT cities.id, cities.name, (SELECT states.name FROM states WHERE states.id = cities.state_id)
+FROM cities
+ORDER BY cities.id ASC;
