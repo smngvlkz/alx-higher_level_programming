7899ffa3649cae19df20861583624c74c895d3d0
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-04 14:24:49 +0200

    cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays all HTTP methods the server will accept for a given URL
-curl -sI "$1" | grep -i "^Allow:" | sed 's/^Allow: //'
+# Script that displays all HTTP methods the server will accept
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2- | tr -d '\r' | sed 's/,//g' | tr ' ' '\n' | sort | tr '\n' ',' | sed 's/,/, /g; s/, $/\n/'
