8232ad792e0170d3ce102ba8442c4771ea4cc7cb
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-05 12:49:09 +0200

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-# Takes in a URL, sends a request to the URL and displays the body of the response"
+# Takes in a URL, sends a request to the URL and displays the body of response"
 import requests
 import sys
 
-def fetch_url(url):
-    try:
-        response = requests.get(url)
-        if response.status_code >= 400:
-            print(f"Error code: {response.status_code}")
-        else:
-            print(response.text)
-    except requests.RequestException as e:
-        print(f"Error: {e}")
-
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-        fetch_url(url)
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
