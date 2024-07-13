6ec6c7f57c164971e8bbaa70b5803a2b478d1a64
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-13 15:50:17 +0200

    From JSON string to Object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
+"""
+This program converts JSON to dictionaries
+"""
+
+
 import json
+
 
 def from_json_string(my_str):
     """
-    Returns an object (Python data strucutre) represented by a JSON string.
-
+    Returns an object (Python data structure) represented by a JSON stringx
     Args:
-        my_str: JSON string to be converted to a Python object.
-
-    Returns:
-        object: Python object represented by `m_str`.
-
-    Raises:
-        ValueError: `my_str` if is not a valid JSON string.
+      - my_str: str
     """
-    return json.loads(my_str)
+    return (json.loads(my_str))
