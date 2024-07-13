99df63cc7f7a1f804018aadbdd6e0e4d2f642e23
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-13 16:16:45 +0200

    Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
+"""
+This program take a file and read it, search a string
+and add other string on the next line
+"""
+
+
 def append_after(filename="", search_string="", new_string=""):
     """
-    Inserts a line of text ti a file, after each line cintaining a specific string.
-
+    Open and reads a file, search a string in all the file.
+    and after each ocurrence add other string (new_string)
     Args:
-        filename: Name of the file.
-        search_string: String to be searched for.
-        new_string: New string to be inserted after each line containing the search string.
+    - filename: string
+    - search_string: string
+    - new_string: string
     """
-    with open(filename, 'r+') as f:
-        lines = f.readlines()
-        for i, line in enumerate(lines):
-            if search_string in line:
-                lines[i] = line + new_string
-        f.seek(0)
-        f.writelines(lines)
+
+    with open(filename, mode="r+", encoding="utf-8") as _file:
+        all_text = _file.readlines()
+        new_text = ""
+
+        for line in all_text:
+            new_text += line
+            if (search_string in line):
+                new_text += new_string
+
+        # This method position the cursor to the beginning of the file
+        _file.seek(0)
+        _file.write(new_text)
