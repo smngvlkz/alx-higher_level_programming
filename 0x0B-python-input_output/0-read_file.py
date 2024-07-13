2a1a980b181db98b21a45ec53e8237d7602fa017
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-13 15:37:47 +0200

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
+"""
+This program reads files
+"""
+
+
 def read_file(filename=""):
     """
-    Reads a text file(UTF8) and prints it ti stdout.
-
-    Args:
-        filename: Name of the file to be read. Defaults to an empty
-        string.
-
-    Raises:
-        FileNotFoundError: the file if it doesn't exist
-        IOError: the file if can't be opened.
+    This function read a file and print it's content
     """
-    with open(filename, 'r', encoding='utf-8') as f:
-        print(f.read(), end="")
+
+    with open(filename, encoding="utf-8") as _file:
+        print(_file.read(), end="")
