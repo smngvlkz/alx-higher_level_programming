997c29ec671c1fce713795deb9c55127223ddeb2
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-14 12:06:43 +0200

    Sinple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-class Rectangle:
-    """
-    Empty class Rectangle that defines a rectangle.
-    """
+"""This program create an empty Rectangle Class"""
+
+
+class Rectangle():
+    """Do nothing"""
     pass
