36f7c070599f1e45d15433ff756e3032734d96b7
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-30 12:46:56 +0200

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,52 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
+if (process.argv.length < 3) {
+  console.error('Usage: ./101-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
-    }
+    console.error('Error:', error);
+    return;
   }
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch data:', response.statusCode);
+    return;
+  }
+
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+
+  const printCharacter = (url, callback) => {
+    request(url, (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error('Error:', charError);
+        return;
+      }
+
+      if (charResponse.statusCode !== 200) {
+        console.error('Failed to fetch data:', charResponse.statusCode);
+        return;
+      }
+
+      const characterData = JSON.parse(charBody);
+      console.log(characterData.name);
+      callback();
+    });
+  };
+
+  const printCharactersInOrder = (index) => {
+    if (index === characters.length) return;
+    printCharacter(characters[index], () => printCharactersInOrder(index + 1));
+  };
+
+  printCharactersInOrder(0);
 });
+
