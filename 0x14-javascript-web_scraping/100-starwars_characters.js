b88a1e36c6a91f28373458047871e796523c3f24
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-30 12:27:56 +0200

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,22 +4,24 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
+request(url, (error, response, body) => {
+    if (error) {
+        console.error(error);
+        return;
     }
-  }
+
+    const jsonData = JSON.parse(body);
+    const charactersUrls = jsonData.characters;
+
+    charactersUrls.forEach((characterUrl) => {
+        request(characterUrl, (error, response, body) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+
+            const characterData = JSON.parse(body);
+            console.log(characterData.name);
+        });
+    });
 });
